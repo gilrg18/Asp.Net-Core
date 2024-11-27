f52662048ecf31b46b425ec2e97c4782c95cd418
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-11-27 11:18:34 -0700

    HTTPS - HTTP Response Headers

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,9 +4,15 @@ var app = builder.Build();
 //app.MapGet("/", () => "Hello World!");
 app.Run(async (HttpContext context) =>
 {
-    context.Response.StatusCode = 400;
-    await context.Response.WriteAsync("Hello (this is the response body)");
-    await context.Response.WriteAsync("World");
+    context.Response.StatusCode = 200;
+
+    context.Response.Headers["MyKey"] = "My Value";
+
+    context.Response.Headers["Server"] = "Gil Server";
+
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync("<h1>Hello </h1>");
+    await context.Response.WriteAsync("<h2>World!</h2>");
 });
 
 app.Run();
