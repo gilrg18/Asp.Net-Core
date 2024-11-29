b2dbc09261b960d5596d0bea0f93ccb6e12ecd88
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-11-29 16:11:30 -0700

    Custom Middleware Class

--- MiddlewareExample/MiddlewareExample/Program.cs
+++ MiddlewareExample/MiddlewareExample/Program.cs
@@ -1,4 +1,8 @@
+using MiddlewareExample.CustomMiddleware;
+
 var builder = WebApplication.CreateBuilder(args);
+//Use your custom Middleware
+builder.Services.AddTransient<MyCustomMiddleware>();
 
 //Application builder object is used to enable or create middleware
 var app = builder.Build();
@@ -6,19 +10,17 @@ var app = builder.Build();
 //Middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync("Middleware 2 ");
     await next(context); //to call the subsequent middleware 
 });
 
-//Middleware 2
-app.Use(async (HttpContext context, RequestDelegate next) =>
-{
-    await context.Response.WriteAsync("Middleware 2");
-    await next(context);
-});
+//Middleware 2 - CustomMiddleware. lets say your middleware has a lot of logic so you create
+//it in another file to avoid big files and confusion
+app.UseMiddleware<MyCustomMiddleware>();
 
+//Middleware 3 (App.Run is a terminating/short-circuit middleware)
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Middleware 3");
+    await context.Response.WriteAsync("Middleware 3 ");
 });
 app.Run();
