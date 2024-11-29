b074c094a57ef9862aed5d4f155b73f0c8a4c66f
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-11-29 16:49:32 -0700

    UseWhen

--- MiddlewareExample/MiddlewareExample/Program.cs
+++ MiddlewareExample/MiddlewareExample/Program.cs
@@ -1,35 +1,23 @@
-using MiddlewareExample.CustomMiddleware;
 
 var builder = WebApplication.CreateBuilder(args);
-//Use your custom Middleware
-builder.Services.AddTransient<MyCustomMiddleware>();
 
-//Application builder object is used to enable or create middleware
 var app = builder.Build();
 
-//Middleware 1
-app.Use(async (HttpContext context, RequestDelegate next) =>
+app.UseWhen(
+    context => context.Request.Query.ContainsKey("username"),
+    app =>
+    {
+        app.Use(async (context, next) =>
+        {
+            await context.Response.WriteAsync("Hello from Middleware branch! \n Username found \n");
+            await next();
+        });
+    }
+    );
+
+app.Run(async (context) =>
 {
-    await context.Response.WriteAsync("Middleware 1 \n");
-    await next(context); //to call the subsequent middleware 
+    await context.Response.WriteAsync("Hello from Middleware at main chain! \n");
 });
 
-//Middleware 2 - CustomMiddleware. lets say your middleware has a lot of logic so you create
-//it in another file to avoid big files and confusion
-//app.UseMiddleware<MyCustomMiddleware>();
-
-
-//Instead of using 'app.UseMiddleware<MyCustomMiddleware>();' in Program.cs we created an extension
-//method to use it there (UseMyCustomMiddleware()) and call it here.
-//UseMyCustomMiddleware is an extension method because we are able to dynamically inject/add a new method
-//into an existing object or type
-//app.UseMyCustomMiddleware();
-
-app.UseHelloCustomMiddleware();
-
-//Middleware 3 (App.Run is a terminating/short-circuit middleware)
-app.Run(async (HttpContext context) =>
-{
-    await context.Response.WriteAsync("Middleware 3 \n");
-});
 app.Run();
