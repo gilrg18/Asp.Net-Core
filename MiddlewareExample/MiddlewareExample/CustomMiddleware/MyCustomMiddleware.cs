5f4b90c639b897fbd64221909faece793405be70
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-11-29 16:24:20 -0700

    Custom Middleware Extensions

--- MiddlewareExample/MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
+++ MiddlewareExample/MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -1,16 +1,28 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 namespace MiddlewareExample.CustomMiddleware
 {
     public class MyCustomMiddleware : IMiddleware
     {
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            await context.Response.WriteAsync("My Custom Middleware - Starts");
+            await context.Response.WriteAsync("My Custom Middleware - Starts\n");
 
             await next(context);
 
-            await context.Response.WriteAsync("My Custom Middleware - Ends");
+            await context.Response.WriteAsync("My Custom Middleware - Ends\n");
 
+        }
+    }
+
+    public static class CustomMiddlewareExtension
+    {
+        //must be static class and method so it is an extension
+        //Prefix with 'Use' is a convention
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MyCustomMiddleware>();
         }
     }
 }
--- MiddlewareExample/MiddlewareExample/Program.cs
+++ MiddlewareExample/MiddlewareExample/Program.cs
@@ -10,17 +10,24 @@ var app = builder.Build();
 //Middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Middleware 2 ");
+    await context.Response.WriteAsync("Middleware 1 \n");
     await next(context); //to call the subsequent middleware 
 });
 
 //Middleware 2 - CustomMiddleware. lets say your middleware has a lot of logic so you create
 //it in another file to avoid big files and confusion
-app.UseMiddleware<MyCustomMiddleware>();
+//app.UseMiddleware<MyCustomMiddleware>();
+
+
+//Instead of using 'app.UseMiddleware<MyCustomMiddleware>();' in Program.cs we created an extension
+//method to use it there (UseMyCustomMiddleware()) and call it here.
+//UseMyCustomMiddleware is an extension method because we are able to dynamically inject/add a new method
+//into an existing object or type
+app.UseMyCustomMiddleware();
 
 //Middleware 3 (App.Run is a terminating/short-circuit middleware)
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Middleware 3 ");
+    await context.Response.WriteAsync("Middleware 3 \n");
 });
 app.Run();
