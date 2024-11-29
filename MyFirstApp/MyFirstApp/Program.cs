c1879e7a919b89eeee58ed64020d6784d68efb9e
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-11-29 11:24:14 -0700

    Assignment 4 Calculator with query params

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,32 +1,68 @@
+using Microsoft.Extensions.Primitives;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 app.Run(async (HttpContext context) =>
 {
-    context.Response.StatusCode = 200;
+    string firstNumberQuery = context.Request.Query["firstNumber"];
+    string secondNumberQuery = context.Request.Query["secondNumber"];
+    string operation = context.Request.Query["operation"];
 
-    context.Response.Headers["MyKey"] = "My Value";
-
-    context.Response.Headers["Server"] = "Gil Server";
-
-    string path = context.Request.Path;
-    string method = context.Request.Method;
-
-    context.Response.Headers["Content-Type"] = "text/html";
-
-
-    if (context.Request.Headers.ContainsKey("AuthorizationCustomKey"))
+    var errorMessages = new List<string>();
+    if (!int.TryParse(firstNumberQuery, out int firstNumber))
     {
-        string key = context.Request.Headers["AuthorizationCustomKey"];
-        await context.Response.WriteAsync($"<p>{key}</p>");
+        errorMessages.Add("Invalid input for 'firstNumber'.");
+    }
+
+    if (!int.TryParse(secondNumberQuery, out int secondNumber))
+    {
+        errorMessages.Add("Invalid input for 'secondNumber'.");
+    }
+
+    string[] validOperations = { "add", "subtract", "multiply", "divide", "module" };
+    if (string.IsNullOrEmpty(operation) || !validOperations.Contains(operation))
+    {
+        errorMessages.Add("Invalid input for 'operation'.");
     }
 
 
-    await context.Response.WriteAsync($"<h1>{path}</h1>");
-    await context.Response.WriteAsync($"<h1>{method}</h1>");
-    await context.Response.WriteAsync("<h1>Hello </h1>");
-    await context.Response.WriteAsync("<h2>World!</h2>");
+    if (errorMessages.Count > 0)
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync(string.Join("\n", errorMessages));
+        return;
+    }
+
+    switch (operation)
+    {
+        case "add":
+            await context.Response.WriteAsync($"Result: {firstNumber + secondNumber}");
+            break;
+
+        case "subtract":
+            await context.Response.WriteAsync($"Result: {firstNumber - secondNumber}");
+            break;
+
+        case "multiply":
+            await context.Response.WriteAsync($"Result: {firstNumber * secondNumber}");
+            break;
+
+        case "divide":
+            await context.Response.WriteAsync($"Result: {firstNumber / secondNumber}");
+            break;
+
+        case "module":
+            await context.Response.WriteAsync($"Result: {firstNumber % secondNumber}");
+            break;
+
+        default:
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("Invalid input for 'operation'");
+            break;
+    }
+
 });
 
 app.Run();
