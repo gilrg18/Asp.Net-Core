8d5eaf16c0b29a55d0dfd0f4ec07ff5e7ae9c7f2
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-02 16:23:39 -0700

    Route Parameters

--- RoutingExample/RoutingExample/Program.cs
+++ RoutingExample/RoutingExample/Program.cs
@@ -3,53 +3,28 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!"); //default endpoint
 
-app.Use(async (context, next) =>
-{
-    //By the time we use this GetEndpoint UseRouting hasnt executed so the
-    //endpoint waqs not recognized and this returns null
-    //Add '?' nullable type because endPoint might be null and here it WILL be null
-    Microsoft.AspNetCore.Http.Endpoint? endPoint = context.GetEndpoint();
-    if (endPoint != null)
-    {
-        await context.Response.WriteAsync($"Endpoint null: {endPoint.DisplayName}\n");
 
-    }
-    await next(context);
-});
 //Enable Routing
 app.UseRouting(); //Selects endpoint
 
-app.Use(async (context, next) =>
-{
-    Microsoft.AspNetCore.Http.Endpoint? endPoint = context.GetEndpoint();
-    if (endPoint != null)
-    {
-        await context.Response.WriteAsync($"Endpoint: {endPoint.DisplayName}\n");
-
-    }
-    await next(context);
-});
 
 #pragma warning disable ASP0014 // Suggest using top level route registrations
 app.UseEndpoints(endpoints => //Executes endpoint
 {
-    //add your endpoints
-    //endpoints.map(url, request delegate (request delegate means a middleware))
-    endpoints.Map("/map1", async (context) =>
+    endpoints.Map("files/{filename}.{extension}", async (context) =>
     {
-        await context.Response.WriteAsync("Inside Map 1");
+        //string? because we are ready to accept null values
+        string? filename = Convert.ToString(context.Request.RouteValues["filename"]);
+        string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
+        await context.Response.WriteAsync($"In Files {filename} / {extension}");
     });
 
-    endpoints.MapGet("/map2", async (context) =>
+    //route names are case insensitive - doesnt matter if its upper or lower case
+    endpoints.Map("employee/profile/{employeename}", async (context) =>
     {
-        await context.Response.WriteAsync("Inside Map 2");
+        string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
+        await context.Response.WriteAsync($"Employee profile: {employeeName}");
     });
-
-    endpoints.MapPost("/map3", async (context) =>
-    {
-        await context.Response.WriteAsync("Inside Map 3");
-    });
-
 
 });
 #pragma warning restore ASP0014 // Suggest using top level route registrations
