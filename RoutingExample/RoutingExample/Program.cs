36ae23fa88bbd2a3ceb8a89a8b671f3f2eee6a94
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-02 15:42:22 -0700

    GetEndPoint

--- RoutingExample/RoutingExample/Program.cs
+++ RoutingExample/RoutingExample/Program.cs
@@ -3,9 +3,34 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!"); //default endpoint
 
+app.Use(async (context, next) =>
+{
+    //By the time we use this GetEndpoint UseRouting hasnt executed so the
+    //endpoint waqs not recognized and this returns null
+    //Add '?' nullable type because endPoint might be null and here it WILL be null
+    Microsoft.AspNetCore.Http.Endpoint? endPoint = context.GetEndpoint();
+    if (endPoint != null)
+    {
+        await context.Response.WriteAsync($"Endpoint null: {endPoint.DisplayName}\n");
+
+    }
+    await next(context);
+});
 //Enable Routing
 app.UseRouting(); //Selects endpoint
 
+app.Use(async (context, next) =>
+{
+    Microsoft.AspNetCore.Http.Endpoint? endPoint = context.GetEndpoint();
+    if (endPoint != null)
+    {
+        await context.Response.WriteAsync($"Endpoint: {endPoint.DisplayName}\n");
+
+    }
+    await next(context);
+});
+
+#pragma warning disable ASP0014 // Suggest using top level route registrations
 app.UseEndpoints(endpoints => //Executes endpoint
 {
     //add your endpoints
@@ -27,6 +52,7 @@ app.UseEndpoints(endpoints => //Executes endpoint
 
 
 });
+#pragma warning restore ASP0014 // Suggest using top level route registrations
 
 //if request is other than map1 or map2 or the default one /
 app.Run(async context =>
