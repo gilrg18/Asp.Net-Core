a49e464eb2e0d96311ab7c4e7a4adf1e9ec17cc3
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-12 16:03:02 -0700

    Multiple Action Methods

--- ControllersExample/ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -2,12 +2,26 @@
 
 namespace ControllersExample.Controllers
 {
-    public class HomeController 
+    public class HomeController
     {
-        [Route("sayhello")] //Attribute Routing "Route" is an attribute, route template as an argument
-       public string method1()
+        [Route("home")] //Attribute Routing "Route" is an attribute, route template as an argument
+        [Route("/")]
+        public string Index() //Action Method
         {
-            return "Hello from method1";
+            return "Hello from Index";
+        }
+
+        [Route("about")]
+        public string About() //Action Method
+        {
+            return "Hello from About";
+        }
+
+        [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")] //url and action method can be named differently
+        public string Contact([FromRoute] string mobile) // Binds the "mobile" route value directly to the parameter
+                                                         //Action Method
+        {
+            return $"Hello from Contact {mobile}";
         }
     }
 }
