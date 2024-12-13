9af95dcf2d94e169fd32e38379af21286d4f3f8b
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-13 15:00:00 -0700

    Assignment 10 - Bank app using controllers

---
+++ IActionResultExample/IActionResultExample/Controllers/BankController.cs
@@ -0,0 +1,50 @@
+﻿using IActionResultExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+   
+    public class BankController : Controller
+    {
+        private Account acc = new Account()
+        {
+            AccountNumber = 1001,
+            AccountHolderName = "Example Name",
+            CurrentBalance = 5000,
+        };
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Content("<h1>Welcome to the best Bank</h1>", "text/html");
+        }
+
+        [Route("/account-details")]
+        public IActionResult Details()
+        {
+            
+            return new JsonResult(acc);
+        }
+
+        [Route("/account-statement")]
+        public IActionResult Statement()
+        {
+            
+            return new VirtualFileResult("/DummyFile.pdf", "application/pdf");
+        }
+
+        [Route("/get-current-balance/{accountNumber:int?}")]
+        public IActionResult Balance(int? accountNumber)
+        {
+            if (accountNumber==null)
+            {
+                return NotFound("Account Number should be supplied");
+            }
+            if (accountNumber != 1001)
+            {
+                return BadRequest("Account Number should be 1001");
+            }
+            return Content(acc.CurrentBalance.ToString(), "text/plain");
+        }
+    }
+}
---
+++ IActionResultExample/IActionResultExample/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace IActionResultExample.Models
+{
+    public class Account
+    {
+        public int AccountNumber { get; set; }
+        public string? AccountHolderName { get; set; }
+        public decimal CurrentBalance { get; set; }
+    }
+}
