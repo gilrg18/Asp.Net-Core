3d2acf763cfdf05e06031cda74299ae8956188f8
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-13 11:39:17 -0700

    Status Code Results

--- IActionResultExample/IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -10,15 +10,19 @@ namespace IActionResultExample.Controllers
             //Book id should be supplied
             if (!Request.Query.ContainsKey("bookid"))
             {
-                Response.StatusCode = 400; //bad request
-                return Content("Book id is not supplied.");
+                //Response.StatusCode = 400; //bad request
+                //return Content("Book id is not supplied.");
+                //Instead of writing the above 2 statements you can write:
+                return BadRequest("Book id is not supplied.");
             }
 
             //Book id can't be empty
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
             {
-                Response.StatusCode = 400; //bad request
-                return Content("Book id can't be null or empty.");
+                //Response.StatusCode = 400; //bad request
+                //return Content("Book id can't be null or empty.");
+                return BadRequest("Book id can't be null or empty.");
+
             }
 
             //Book id should be between 1 to 1000
@@ -28,19 +32,25 @@ namespace IActionResultExample.Controllers
             int bookId = Convert.ToInt16(ControllerContext.HttpContext.Request.Query["bookid"]);
             if (bookId <= 0)
             {
-                Response.StatusCode = 400; //bad request
-                return Content("Book id cant be less than or equal to zero");
+                //Response.StatusCode = 400; //bad request
+                //return Content("Book id cant be less than or equal to zero");
+                return BadRequest("Book id cant be less than or equal to zero");
+
             }
-            if(bookId > 1000)
+            if (bookId > 1000)
             {
-                Response.StatusCode = 400; //bad request
-                return Content("Book id cant be greater than 1000");
+                //Response.StatusCode = 400; //bad request
+                //return Content("Book id cant be greater than 1000");
+                return NotFound("Book not found!"); //404
+                //The difference between NotFound and NotFoundResult is the error message
+                //you pass into the method
             }
 
             if (!Convert.ToBoolean(Request.Query["isloggedin"]))
             {
-                Response.StatusCode = 401; //unauthorized
-                return Content("User must be authenticated");
+                //Response.StatusCode = 401; //unauthorized
+                //return Content("User must be authenticated");
+                return Unauthorized("User must be authenticated");
             }
             return File("/sample.pdf", "application/pdf");
         }
