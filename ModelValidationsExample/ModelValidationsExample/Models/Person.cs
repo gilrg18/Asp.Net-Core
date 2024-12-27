412a52895271c91c311dd153df92902c4de2224c
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-26 17:20:08 -0700

    Introduction to Model Validations

---
+++ ModelValidationsExample/ModelValidationsExample/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ModelValidationsExample.Models
+{
+    public class Person
+    {
+        [Required] //this indicates the property Name is mandatory, cannot be null or empty
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object - Name: {Name}, Email: {Email}, Phone: {Phone}, Password: {Password}, Price: {Price}";
+        }
+    }
+}
