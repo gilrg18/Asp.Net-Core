7a7b1d36efeb9f81bb4f5270fb766a8c37209b42
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-26 18:48:47 -0700

    All model validations

--- ModelValidationsExample/ModelValidationsExample/Models/Person.cs
+++ ModelValidationsExample/ModelValidationsExample/Models/Person.cs
@@ -1,19 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 namespace ModelValidationsExample.Models
 {
     public class Person
     {
-        [Required(ErrorMessage = "Please provide name :)")] //this indicates the property Name is mandatory, cannot be null or empty
+        [Required(ErrorMessage = "{0} can't be empty or null")] //this indicates the property Name is mandatory, cannot be null or empty
+        [Display(Name="Person Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage="{0} should be between {2} to {1} characters long")]
+        [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should contain only alphabets, space and dot")] //accept only letter and .
         public string? Name { get; set; }
+        [EmailAddress(ErrorMessage = "{0} should be a proper email address")]
+        [Required(ErrorMessage ="{0} can't be blank")]
         public string? Email { get; set; }
+        [Phone(ErrorMessage ="{0} should contain 10 digits")]
+        [ValidateNever]
         public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
         public string? Password { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Compare("Password", ErrorMessage = "{0} and {1} do not match")]
+        [Display(Name="Password Confirmation")]
         public string? ConfirmPassword { get; set; }
-        public string? Price { get; set; }
+        [Range(0,999.99, ErrorMessage ="{0} should be between {1} and {2}")]
+        public double? Price { get; set; }
 
         public override string ToString()
         {
-            return $"Person object - Name: {Name}, Email: {Email}, Phone: {Phone}, Password: {Password}, Price: {Price}";
+            return $"Person object - Name: {Name}, Email: {Email}, Phone: {Phone}, Password: {Password},ConfirmPassword: {ConfirmPassword}, Price: {Price}";
         }
     }
 }
