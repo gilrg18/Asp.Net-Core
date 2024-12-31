5b596dbe3db7f5029ed9d23a44549f179f09e3b1
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-31 14:28:29 -0700

    Code Blocks and Expressions

--- ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ namespace ViewsExample.Controllers
     public class HomeController : Controller
     {
         [Route("home")]
+        [Route("/")]
         public IActionResult Index()
         {
             return View(); //if you dont specify the view name it will take /Views/Home/Index.cshtml because of the method name
