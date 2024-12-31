26d0785f6a10a8f5a1ff81b7fd368ae9d458aac1
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-31 14:04:35 -0700

    Razor Views [MVC] - Views

---
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View(); //if you dont specify the view name it will take /Views/Home/Index.cshtml because of the method name
+            //return View("abcView"); //abcView.chstml
+            //return new ViewResult() { ViewName = "abcView" };
+        }
+    }
+}
