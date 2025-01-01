b58f15523e161b06d5b1be272c8de4f804adb2f1
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-12-31 21:39:21 -0700

    ViewBag

--- ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace ViewsExample.Controllers
                 new Person() {Name="Gil",DateOfBirth = DateTime.Parse("2000-01-01"), PersonGender = Gender.Male},
                 new Person() {Name="Gil",DateOfBirth = null, PersonGender = Gender.Male},
             };
-            ViewData["people"] = people;
+            //ViewData["people"] = people;
+            ViewBag.people = people; //heres no benefit, just syntactic sugar
 
             return View(); //if you dont specify the view name it will take /Views/Home/Index.cshtml because of the method name
             //return View("abcView"); //abcView.chstml
