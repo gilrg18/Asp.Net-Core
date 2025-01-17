9c91533971a7a2de58183aaf593b599c34090f82
gilrg18 <gilberto_rogel_26@hotmail.com>
2025-01-16 19:53:20 -0700

    Dependency Injection - Services - Part 1

---
+++ DIExample/Services/CitiesService.cs
@@ -0,0 +1,26 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities = new List<string>();
+
+        public CitiesService()
+        {
+            //In real world proyects we will invoke the data layer to fetch this data
+            //from the database
+            _cities = new List<string>
+            {
+                "London",
+                "Paris",
+                "Mazatlan",
+                "Culiacan",
+                "Chiapas"
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}
