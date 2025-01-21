0f0d878e27cbc74ccc6c8cb0fb2f5c51aa05f3e3
gilrg18 <gilberto_rogel_26@hotmail.com>
2025-01-21 13:45:37 -0700

    AddTrnasient, AddScopr, AddSingleton()

--- DIExample/DIExample/Program.cs
+++ DIExample/DIExample/Program.cs
@@ -3,12 +3,16 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.Add(new ServiceDescriptor
-(
-    typeof(ICitiesService),
-    typeof(CitiesService),
-    ServiceLifetime.Scoped
-));
+//builder.Services.Add(new ServiceDescriptor
+//(
+//    typeof(ICitiesService),
+//    typeof(CitiesService),
+//    ServiceLifetime.Transient
+//));
+//builder.Services.AddTransient<ICitiesService, CitiesService>();
+builder.Services.AddScoped<ICitiesService, CitiesService>(); //You can use this shorthand method instead of the above one
+//builder.Services.AddSingleton<ICitiesService, CitiesService>();
+
 
 
 var app = builder.Build();
