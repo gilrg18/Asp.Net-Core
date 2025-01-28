98a8c3805f2fb86d38fa12acce09af6d385aaa47
gilrg18 <gilberto_rogel_26@hotmail.com>
2025-01-28 14:35:57 -0700

    Environment in Controller

--- EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
+++ EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
@@ -4,18 +4,23 @@ namespace EnvironmentsExample.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;   
+        }
+
         [Route("/")]
         [Route("some-route")]
         public IActionResult Index()
         {
+            if(_webHostEnvironment.IsDevelopment()){
+                ViewBag.CurrentEnvironment = _webHostEnvironment.EnvironmentName;
+            }
             return View();
         }
 
-        //Purposely causing an exception to show dev exception page
-        [Route("some-route")]
-        public IActionResult Other()
-        {
-            return View();
-        }
     }
 }
