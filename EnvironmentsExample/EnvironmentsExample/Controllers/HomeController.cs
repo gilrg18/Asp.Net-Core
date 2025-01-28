c7fc344021e777ad55a4cf29b60026f188a796fb
gilrg18 <gilberto_rogel_26@hotmail.com>
2025-01-28 14:27:31 -0700

    13-Environments - Environment In Launch Settings

---
+++ EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("some-route")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Purposely causing an exception to show dev exception page
+        [Route("some-route")]
+        public IActionResult Other()
+        {
+            return View();
+        }
+    }
+}
