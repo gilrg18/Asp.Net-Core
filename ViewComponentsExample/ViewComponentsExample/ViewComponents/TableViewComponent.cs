12114cd86d234e23f390ea3201a0ddb3a2021811
gilrg18 <gilberto_rogel_26@hotmail.com>
2025-01-29 16:18:31 -0700

    View Components with Parameters

--- ViewComponentsExample/ViewComponentsExample/ViewComponents/TableViewComponent.cs
+++ ViewComponentsExample/ViewComponentsExample/ViewComponents/TableViewComponent.cs
@@ -6,19 +6,19 @@ namespace ViewComponentsExample.ViewComponents
     //[ViewComponent] //Optional Atribute
     public class TableViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync() //green underline because we are not using an await keyword in the logic
+        public async Task<IViewComponentResult> InvokeAsync(PersonTableModel table) //green underline because we are not using an await keyword in the logic
         {
-            PersonTableModel personTableModel = new PersonTableModel()
-            {
-                TableTitle = "Persons List",
-                Persons = new List<Person>() {
-                    new Person() { Name = "Gil", JobTitle =  "Programmer" },
-                    new Person() { Name = "Gil2", JobTitle =  "Programmer2" },
-                    new Person() { Name = "Gil3", JobTitle =  "Programmer3" }
-                }
-            };
+            //PersonTableModel personTableModel = new PersonTableModel()
+            //{
+            //    TableTitle = "Persons List",
+            //    Persons = new List<Person>() {
+            //        new Person() { Name = "Gil", JobTitle =  "Programmer" },
+            //        new Person() { Name = "Gil2", JobTitle =  "Programmer2" },
+            //        new Person() { Name = "Gil3", JobTitle =  "Programmer3" }
+            //    }
+            //};
             //ViewBag.Table = model;
-            return View("Sample", personTableModel); //invoked a partial view Views/Shared/Components/Table/Default.cshtml
+            return View("Sample", table); //invoked a partial view Views/Shared/Components/Table/Sample.cshtml
         }
     }
 }
