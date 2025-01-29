f34fd6a18b683c84596a751ba923b090241db0a4
gilrg18 <gilberto_rogel_26@hotmail.com>
2025-01-29 12:19:39 -0700

    Creating View Components - Part 1

---
+++ ViewComponentsExample/ViewComponentsExample/ViewComponents/TableViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentsExample.ViewComponents
+{
+    //[ViewComponent] //Optional Atribute
+    public class TableViewComponent : ViewComponent
+    {
+    }
+}
