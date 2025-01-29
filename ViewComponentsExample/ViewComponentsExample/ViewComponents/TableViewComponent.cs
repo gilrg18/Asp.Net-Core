32140a1e079b9c14975f98378e1d57263204b761
gilrg18 <gilberto_rogel_26@hotmail.com>
2025-01-29 12:39:17 -0700

    Section  10 - Creating View Components - Part 2

--- ViewComponentsExample/ViewComponentsExample/ViewComponents/TableViewComponent.cs
+++ ViewComponentsExample/ViewComponentsExample/ViewComponents/TableViewComponent.cs
@@ -5,5 +5,9 @@ namespace ViewComponentsExample.ViewComponents
     //[ViewComponent] //Optional Atribute
     public class TableViewComponent : ViewComponent
     {
+        public async Task<IViewComponentResult> InvokeAsync() //green underline because we are not using an await keyword in the logic
+        {
+            return View(); //invoked a partial view Views/Shared/Components/Table/Default.cshtml
+        }
     }
 }
